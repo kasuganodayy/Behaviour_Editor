5914f0f091d7b9fbdd9fcdaa0544b99b4540394b
Chow Damien <dayy124@gmail.com>
2017-01-18 22:46:27 +0900

    Initial

---
+++ Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Behaviour_Editor
+{
+    public class Class1
+    {
+        public void SerializeTest()
+        {
+            // serialize
+            Npc testNPC = new Npc();
+            testNPC.name = "JoeTheDayGuard";
+            testNPC.pos.x = -16;
+            testNPC.pos.y = 25;
+            testNPC.facing.x = 1;
+            testNPC.facing.y = 0;
+            testNPC.colour.r =
+                testNPC.colour.g =
+                testNPC.colour.b = 0;
+            testNPC.colour.a = 255;
+            testNPC.objectOwned = new List<string>(){
+                new string();
+            }
+        }
+    }
+
+    public class Npc
+    {
+        [XmlAttribute("Name")]
+        public string name{ get; set; }
+        [XmlAttribute("Shape")]
+        public Vector2 pos { get; set; }
+        [XmlAttribute("Facing")]
+        public Vector2 facing { get; set; }
+        [XmlAttribute("Color")]
+        public Colour colour { get; set; }
+        [XmlAttribute("ObjectOwnership")]
+        public List<string> objectOwned { get; set; }
+        [XmlAttribute("InitialSchedule")]
+        public string actionSchedule { get; set; }
+    }
+
+    public class Vector2
+    {
+        [XmlAttribute]
+        public decimal x;
+        public decimal y;
+    }
+
+    public class Colour
+    {
+        [XmlAttribute]
+        public int r;
+        public int g;
+        public int b;
+        public int a;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Behaviour_Editor
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = null;
+
+        public Form1()
+        {
+            filePath = "xml/Objects.xml";
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataSet1.ReadXml(filePath);
+
+            dataGridView1.DataSource = dataSet1;
+            //dataGridView1.DataMember = "authors";
+            dataGridView1.DataMember = "Npc";
+
+            bool pause;
+            pause = true;
+            //dataGridView1.Columns.Remove("contract");
+            //AddComboBoxColumn();
+        }
+
+        //private void AddComboBoxColumn()
+        //{
+        //    DataGridViewComboBoxColumn comboboxColumn;
+
+        //    comboboxColumn = new DataGridViewComboBoxColumn();
+        //    comboboxColumn.DataPropertyName = "contract";
+        //    comboboxColumn.HeaderText = "Contracted";
+        //    comboboxColumn.DropDownWidth = 100;
+        //    comboboxColumn.Width = comboboxColumn.DropDownWidth;
+        //    comboboxColumn.MaxDropDownItems = 2;
+        //    comboboxColumn.FlatStyle = FlatStyle.Flat;
+        //    comboboxColumn.Name = "contract";
+
+        //    comboboxColumn.Items.AddRange("True", "False");
+        //    comboboxColumn.ValueType = Type.GetType("bool");
+
+        //    dataGridView1.Columns.Add(comboboxColumn);
+        //}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Click -= button1_Click;
+
+            // Save XML
+            System.IO.FileStream stream = new System.IO.FileStream(filePath, System.IO.FileMode.Create);
+            dataSet1.WriteXml(stream);
+
+            button1.Click += button1_Click;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Click -= button2_Click;
+
+            // Serialize XML
+            Class1 test = new Class1();
+            test.SerializeTest();
+
+            button2.Click += button2_Click;
+        }
+    }
+}
